78981a2f11db615fa8f47e92d4febb9d11be153b
sedaisagar <sagarsedai1997@gmail.com>
2026-01-19 08:05:37 +0545

    update::>>api

--- api/serializers/categories.py
+++ api/serializers/categories.py
@@ -5,4 +5,26 @@ from products.models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields="__all__"
+        # fields=['id', 'name', 'slug', 'created_at', 'updated_at']
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        cats = instance.children.all()
+        if cats.exists():
+            data["categories"] = CategorySerializer(cats, many=True).data
+        return data
+    
+    def validate(self, attrs):
+        return super().validate(attrs)
+    
+    def is_valid(self, *, raise_exception=False):
+        return super().is_valid(raise_exception=raise_exception)
+    
+    def create(self, validated_data):
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+    
+    
--- api/viewsets/categories.py
+++ api/viewsets/categories.py
@@ -4,8 +4,33 @@ from api.serializers.categories import CategorySerializer
 from products.models import Category
 
 from drf_spectacular.utils import extend_schema
+from rest_framework.response import Response
+from rest_framework import status
 
 @extend_schema(tags=["Category(s)"])
 class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Category.objects.filter(parent__isnull=True)
+
+        return super().get_queryset()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
